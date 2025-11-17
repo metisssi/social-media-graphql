9cb9acdcf023fa9358627924aadb79c8f3b6ca16
metisssi <simcheraadnrij@gmail.com>
2025-11-17 16:42:25 +0100

    users env change

--- grapql/resolvers/users.js
+++ grapql/resolvers/users.js
@@ -1,11 +1,13 @@
 const bcrypt = require("bcryptjs")
 const jwt = require('jsonwebtoken')
 const { UserInputError } = require('apollo-server');
+require('dotenv').config();
+
 
 const { validateRegisterInput, validateLoginInput } = require('../../util/validators')
 
 
-const { SECRET_KEY } = require('../../config')
+const SECRET_KEY = process.env.SECRET_KEY
 const User = require('../../models/User')
 
 
